a4abb34c7300252fdfc7ae7f830ae3f94cc8b01a
Muqtadir Billah <>
2021-03-30 19:11:04 +0500

    added sub components

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,46 +1,15 @@
 import React from "react"
+import Skill from "./Skill"
 
 function Skills(){
     return(
         <div className="skillsDiv"  id="skills">
             <h1>Dominant Skills</h1>
             <div className="row justify-content-md-center">
-                <div className="skillsCardDiv col-lg-3 col-md-6 col-sm-12">
-                    <div className="card skillsCard" >
-                        <div class="card-body">
-                            <img src="images/mongodb.png" className="skillsImg" alt="" />
-                            <h5 class="card-title">Mongodb</h5>
-                            <p class="card-text">NoSQL Database for saving data</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="skillsCardDiv col-lg-3 col-md-6 col-sm-12">
-                    <div className="card skillsCard" >
-                        <div class="card-body">
-                            <img src="images/express.png" className="skillsImg" alt="" />
-                            <h5 class="card-title">Express.JS</h5>
-                            <p class="card-text">Back end Web App framework for Node.js</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="skillsCardDiv col-lg-3 col-md-6 col-sm-12">
-                    <div className="card skillsCard" >
-                        <div class="card-body">
-                            <img src="images/react.png" className="skillsImg" alt="" />
-                            <h5 class="card-title">React.JS</h5>
-                            <p class="card-text">Front end, JavaScript library for building UI</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="skillsCardDiv col-lg-3 col-md-6 col-sm-12">
-                    <div className="card skillsCard" >
-                        <div class="card-body">
-                            <img src="images/node.png" className="skillsImg" alt="" />
-                            <h5 class="card-title">Node.JS</h5>
-                            <p class="card-text">Back end JavaScript runtime environment.</p>
-                        </div>
-                    </div>
-                </div>
+                <Skill imgName="mongodb.png" title="Mongodb" description="NoSQL Database for saving data" />
+                <Skill imgName="express.png" title="Express.JS" description="Back end Web App framework for Node.js" />
+                <Skill imgName="react.png" title="React.JS" description="Front end, JavaScript library for building UI" />
+                <Skill imgName="node.png" title="Node.JS" description="Back end JavaScript runtime environment." />
             </div>
         </div>
     );
